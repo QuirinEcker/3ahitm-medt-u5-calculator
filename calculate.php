b7040f994efe594507cadb8a7da7a27e4f468479
Quirin <quirin.ecker@bajupa.com>
2019-10-19 18:41:23 +0200

    calculator finished

---
+++ calculate.php
@@ -0,0 +1,25 @@
+<?php
+$calcstring = $_POST["data"];
+$calculationComponents = explode(" ", $calcstring );
+
+try {
+    if (sizeof($calculationComponents) == 3) {
+        switch ($calculationComponents[1]) {
+            case '+': echo $calculationComponents[0] + $calculationComponents[2];
+                break;
+            case '*': echo $calculationComponents[0] * $calculationComponents[2];
+                break;
+            case '/': echo $calculationComponents[0] / $calculationComponents[2];
+                break;
+            case '-': echo $calculationComponents[0] - $calculationComponents[2];
+                break;
+        }
+    } else throw new Exception();
+
+} catch (Exception $e) {
+    echo "ERROR: Only two numbers allowed";
+}
+
+
+
+//echo eval("return $splitter[0];");
